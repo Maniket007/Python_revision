7a03d19799d239741cb5090d07de90226227efbc
Maniket Kumar <sonuanand644@gmail.com>
2022-07-04 11:28:26 +0530

    trailing_fact

---
+++ Python Course with Notes/Mathematics/Trailing_fact.py
@@ -0,0 +1,17 @@
+import re
+
+
+def trailfact(n):
+    fact=1
+    for i in range(1, n+1):
+        fact = fact*i
+    zeroes = 0
+    while(fact%10==0):
+        fact = fact/10
+        zeroes = zeroes+1
+    return zeroes
+num = int(input("Enter a number"))
+if num<0:
+    print("the fact is not exist")
+else:
+    print("the trail_factorial is",trailfact(num))
