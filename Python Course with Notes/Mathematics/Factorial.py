83e8dd12606fbda910c8a613d1a9f5cdc36dd2e3
Maniket Kumar <sonuanand644@gmail.com>
2022-07-04 10:56:45 +0530

    recursive_factorial_n

--- Python Course with Notes/Mathematics/Factorial.py
+++ Python Course with Notes/Mathematics/Factorial.py
@@ -1,12 +1,24 @@
-def get_factorial_for_loop(n):
-    result = 1
-    if n > 1:
-        for i in range(1, n+1):
-            result = result * i
-        return result
-    else:
-        return 'n has to be positive'
-inp = input("Enter a number: ")
-inp = int(inp)
+#Itrerative approach..
+#def get_factorial_for_loop(n):
+#    result = 1
+#     if n > 1:
+#       for i in range(1, n+1):
+#           result = result * i
+#       return result
+#   else:
+#       return 'n has to be positive'
+#inp = input("Enter a number: ")
+#inp = int(inp)
+#print(f"The result is: {get_factorial_for_loop(inp)}") 
 
-print(f"The result is: {get_factorial_for_loop(inp)}") 
+#Recurcive approach..
+def rec(n):
+    if n==0:
+        return 1
+    else:
+        return n*rec(n-1)
+num = int(input("Enter a number"))
+if num<0:
+    print("the fact is not exist")
+else:
+    print("the factorial is",rec(num))
